fd106d1351939d05d02191b942a31f420e8db8a5
adicoolls <160615870+adicoolls@users.noreply.github.com>
2026-02-24 18:07:12 +0530

    Removed node_modules and added gitignore

--- project 2/controller/userController.js
+++ project 2/controller/userController.js
@@ -1,36 +1,96 @@
-const express = require("express");
-const user = require("../models/userModel");
-const mongoose = require("mongoose");
+const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
+
+// 🔹 Create User
 const CreateUser = async (req, res) => {
-    console.log("CreateUser works ");
-    const { Name, Email, Password } = req.body;
-    const User = await user.create({
-        Name, 
-        Email,
-        Password,
-    });
-    res.json(User);
+    try {
+        const { Name, Email, Password } = req.body;
+
+        // Hash password before saving
+        const hashedPassword = await bcrypt.hash(Password, 10);
+
+        const newUser = await User.create({
+            Name,
+            Email,
+            Password: hashedPassword
+        });
+
+        res.status(201).json(newUser);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
+    }
 };
 
-const findallUser = async (req, res) => {
-    console.log("found the all users ");
-    const User = await user.find();
-    res.json(User);
 
-}
-const findOneUser = async (req, res) => {
-    const {Email} = req.query;
-    const User = await user.findOne({Email});
-    if(!user) {
-        return res.status(404).res.send("the user is not found");
+// 🔹 Find All Users
+const findallUser = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
     }
-    res.json(User);
-}
+};
+
+
+// 🔹 Find One User
+const findOneUser = async (req, res) => {
+    try {
+        const { Email } = req.query;
+
+        const user = await User.findOne({ Email });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.json(user);
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+
+// 🔹 Login User
+const loginUser = async (req, res) => {
+    try {
+        const { Email, Password } = req.body;
+
+        const user = await User.findOne({ Email });
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(Password, user.Password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            "mysecretkey",
+            { expiresIn: "1h" }
+        );
+
+        res.status(200).json({ token });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
 
 module.exports = {
     CreateUser,
     findallUser,
     findOneUser,
-}
+    loginUser
+};
--- project 2/routes/userRoutes.js
+++ project 2/routes/userRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {  CreateUser,
     findallUser,
-    findOneUser,} = require("../controller/userController");
+    findOneUser,
+loginUser,} = require("../controller/userController");
 
 const verifyToken = require("../middleware/auth");
 const { createUser } = require("../../project 1/controllers/userControllers");
 
 router.get("/users", verifyToken, findallUser);
-router.post("/create", verifyToken, CreateUser);
+router.post("/create", CreateUser);
+router.post("/login", loginUser);
 
 module.exports = router;
