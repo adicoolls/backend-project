2049e5dfbdb553b8c120cec112efe65413bc123b
adicoolls <160615870+adicoolls@users.noreply.github.com>
2026-03-02 16:42:53 +0530

    .

--- project 2/models/userModel.js
+++ project 2/models/userModel.js
@@ -1,4 +1,4 @@
-const express = require("express");
+
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema ({
--- project 3/server.js
+++
@@ -1,9 +0,0 @@
-// RBAC project
-const express = require("express");
-const mongoose = require("mongoose");
-const PORT = 8000;
-const app = express();
-app.get("/", (req, res) => {
-    console.log("hello world");
-})
-app.listen(PORT, () => console.log(`the server is connected on port ${PORT}`));
